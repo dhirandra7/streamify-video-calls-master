d204ee9d1b57d32002a4da3f394b4d0fe150270b
dhirendra <dhirandrapandey7@gmail.com>
2025-05-24 19:28:16 +0530

    fix

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.MODE === "development" ? "https://streamify-video-calls-master-a1ok.onrender.com/api" : "/api";
+const BASE_URL = "https://streamify-video-calls-master-a1ok.onrender.com/api";
 
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
