bb458abb51bcc85cce604abdbfa7192729531da6
dhirendra <dhirandrapandey7@gmail.com>
2025-05-24 19:29:37 +0530

    fix

--- backend/src/server.js
+++ backend/src/server.js
@@ -25,6 +25,10 @@ app.use(
 app.use(express.json());
 app.use(cookieParser());
 
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/chat", chatRoutes);
